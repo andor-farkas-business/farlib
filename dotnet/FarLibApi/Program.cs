539714fdae3044cb1c8e0012ccfb4564f381bd73
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-10 15:37:45 +0100

    [Added] local MSSQL connection to the Api layer

--- dotnet/FarLibApi/Program.cs
+++ dotnet/FarLibApi/Program.cs
@@ -1,8 +1,17 @@
+using FarLibDAL.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FarLibDbContext>(options =>
+{
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
