6a3287dc0c50d406cc2e49364153de63f5ce8a80
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-15 17:23:50 +0100

    [Added] repository interfaces and implementations for Authors, Books, Distributors and Stocks

---
+++ dotnet/FarLibDAL/Books/Repositories/BookRepository.cs
@@ -0,0 +1,102 @@
+using FarLibCL.Books.Dtos;
+using FarLibCL.Books.Enums;
+using FarLibDAL.Books.Entities;
+using FarLibDAL.Books.Repositories.Interfaces;
+using FarLibDAL.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarLibDAL.Books.Repositories;
+
+public class BookRepository(FarLibDbContext dbContext) : IBookRepository
+{
+    public async Task AddAsync(Book book)
+    {
+        await dbContext.AddAsync(book);
+    }
+
+    public async Task<Book?> GetByIdAsync(Guid id)
+    {
+        return await dbContext.Books
+            .Include(b => b.Authors)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<IList<Book>> GetAllAsync()
+    {
+        return await dbContext.Books
+            .Include(b => b.Authors)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<IList<Book>> GetByTypeAsync(BookType type)
+    {
+        return await dbContext.Books
+            .Include(b => b.Authors)
+            .AsNoTracking()
+            .Where(b => b.Type == type)
+            .ToListAsync();
+    }
+
+    // Contains all flagged categories
+    public async Task<IList<Book>> GetByCategoryAsync(BookCategory category)
+    {
+        return await dbContext.Books
+            .Include(b => b.Authors)
+            .AsNoTracking()
+            .Where(b => b.Category.HasFlag(category))
+            .ToListAsync();
+    }
+
+    public async Task<IList<Book>> GetByAuthorAsync(Guid authorId)
+    {
+        return await dbContext.Books
+            .Include(b => b.Authors)
+            .AsNoTracking()
+            .Where(b => b.Authors.Any(x => x.Id == authorId))
+            .ToListAsync();
+    }
+
+    public async Task UpdateAsync(Guid id, UpdateBookDto update)
+    {
+        var foundBook = await dbContext.Books.FindAsync(id);
+
+        if (foundBook == null)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrWhiteSpace(update.Title))
+        {
+            foundBook.Title = update.Title;
+        }
+
+        if (!string.IsNullOrWhiteSpace(update.Description))
+        {
+            foundBook.Description = update.Description;
+        }
+
+        if (update.Type != null && update.Type.HasValue)
+        {
+            foundBook.Type = update.Type.Value;
+        }
+
+        if (update.Category != null && update.Category.HasValue)
+        {
+            foundBook.Category = update.Category.Value;
+        }
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var foundBook = await dbContext.Books.FindAsync(id);
+
+        if (foundBook == null)
+        {
+            return;
+        }
+
+        dbContext.Books.Remove(foundBook);
+    }
+}
---
+++ dotnet/FarLibDAL/Distributors/Repositories/DistributorRepository.cs
@@ -0,0 +1,76 @@
+using FarLibCL.Distributors.Dtos;
+using FarLibCL.Distributors.Enums;
+using FarLibDAL.Database;
+using FarLibDAL.Distributors.Entities;
+using FarLibDAL.Distributors.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FarLibDAL.Distributors.Repositories;
+
+public class DistributorRepository(FarLibDbContext dbContext) : IDistributorRepository
+{
+    public async Task AddAsync(Distributor distributor)
+    {
+        await dbContext.Distributors.AddAsync(distributor);
+    }
+
+    public async Task<Distributor?> GetByIdAsync(Guid id)
+    {
+        return await dbContext.Distributors
+            .Include(d => d.Stocks)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<IList<Distributor>> GetAllAsync()
+    {
+        return await dbContext.Distributors
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<IList<Distributor>> GetByTypeAsync(DistributorType type)
+    {
+        return await dbContext.Distributors
+            .AsNoTracking()
+            .Where(d => d.Type == type)
+            .ToListAsync();
+    }
+
+    public async Task UpdateAsync(Guid id, UpdateDistributorDto update)
+    {
+        var foundDistributor = await dbContext.Distributors.FindAsync(id);
+
+        if (foundDistributor == null)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrWhiteSpace(update.Name))
+        {
+            foundDistributor.Name = update.Name;
+        }
+
+        if (!string.IsNullOrWhiteSpace(update.Address))
+        {
+            foundDistributor.Address = update.Address;
+        }
+        
+        if (update.Type != null && update.Type.HasValue)
+        {
+            foundDistributor.Type = update.Type.Value;
+        }
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var foundDistributor = await dbContext.Distributors.FindAsync(id);
+
+        if (foundDistributor == null)
+        {
+            return;
+        }
+
+        dbContext.Distributors.Remove(foundDistributor);
+    }
+}
