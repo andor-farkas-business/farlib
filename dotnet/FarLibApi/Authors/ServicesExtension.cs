da39d630aabc897d2ce48e61161d347d98f432af
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-16 15:49:56 +0100

    [Added] IAuthorService and IAuthorMapper to DI container

--- dotnet/FarLibApi/Authors/ServicesExtension.cs
+++ dotnet/FarLibApi/Authors/ServicesExtension.cs
@@ -1,3 +1,7 @@
+using FarLibBLL.Authors.Mappers;
+using FarLibBLL.Authors.Mappers.Interfaces;
+using FarLibBLL.Authors.Services;
+using FarLibBLL.Authors.Services.Interfaces;
 using FarLibDAL.Authors.Repositories;
 using FarLibDAL.Authors.Repositories.Interfaces;
 
@@ -8,5 +12,8 @@ public static class ServicesExtension
     public static void AddAuthorServices(this IServiceCollection services)
     {
         services.AddScoped<IAuthorRepository, AuthorRepository>();
+
+        services.AddScoped<IAuthorService, AuthorService>();
+        services.AddSingleton<IAuthorMapper, AuthorMapper>();
     }
 }
