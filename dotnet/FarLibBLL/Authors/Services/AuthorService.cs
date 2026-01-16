49649a283d0f3bd407ad5bc5671301704a58c3b6
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-16 15:48:46 +0100

    [Added] IAuthorService, IAuthorMapper interfaces, implementation and required dtos

---
+++ dotnet/FarLibBLL/Authors/Services/AuthorService.cs
@@ -0,0 +1,45 @@
+using FarLibBLL.Authors.Mappers.Interfaces;
+using FarLibBLL.Authors.Services.Interfaces;
+using FarLibCL.Authors.Dtos;
+using FarLibCL.Authors.Entities;
+using FarLibCL.Exceptions;
+using FarLibDAL.Authors.Repositories.Interfaces;
+
+namespace FarLibBLL.Authors.Services;
+
+public class AuthorService(
+    IAuthorRepository repository,
+    IAuthorMapper mapper) : IAuthorService
+{
+    public async Task AddAsync(AddAuthorDto add)
+    {
+        var author = mapper.MapAddDtoToEntity(add);
+
+        await repository.AddAsync(author);
+    }
+
+    public async Task<AuthorDetailsDto> GetByIdAsync(Guid id)
+    {
+        var entity = await repository.GetByIdAsync(id) ??
+            throw new ObjectNotFoundException(nameof(Author), nameof(Author.Id), id.ToString());
+
+        return mapper.MapEntityToDetailsDto(entity);
+    }
+
+    public async Task<IList<AuthorListItemDto>> GetAllAsync()
+    {
+        var entities = await repository.GetAllAsync();
+
+        return [.. entities.Select(mapper.MapEntityToListItemDto)];
+    }
+
+    public async Task UpdateAsync(Guid id, UpdateAuthorDto update)
+    {
+        await repository.UpdateAsync(id, update);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await repository.DeleteAsync(id);
+    }
+}
---
+++ dotnet/FarLibCL/Authors/Dtos/AddAuthorDto.cs
@@ -0,0 +1,7 @@
+namespace FarLibCL.Authors.Dtos;
+
+public class AddAuthorDto
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
