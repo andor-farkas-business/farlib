e45a547216239757afb208d6614a96ba25bcb47c
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-16 15:50:11 +0100

    [Added] AuthorsController

---
+++ dotnet/FarLibApi/Authors/AuthorsController.cs
@@ -0,0 +1,51 @@
+using FarLibBLL.Authors.Services.Interfaces;
+using FarLibCL.Authors.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarLibApi.Authors;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class AuthorsController(IAuthorService service) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AddAsync([FromBody] AddAuthorDto add)
+    {
+        await service.AddAsync(add);
+
+        return Created();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuthorDetailsDto>> GetByIdAsync(Guid id)
+    {
+        var author = await service.GetByIdAsync(id);
+
+        return Ok(author);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<AuthorListItemDto>>> GetAllAsync()
+    {
+        var authors = await service.GetAllAsync();
+
+        return Ok(authors);
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateAuthorDto update)
+    {
+        await service.UpdateAsync(id, update);
+
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        await service.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
