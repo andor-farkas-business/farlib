1eb7cc7e7198b4c85a62c0476f7ae37281a4aba9
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-24 18:28:10 +0100

    [Added] IBookService, IBookMapper interfaces, implementation and required dtos

---
+++ dotnet/FarLibBLL/Books/Mappers/BookMapper.cs
@@ -0,0 +1,52 @@
+using FarLibBLL.Books.Mappers.Interfaces;
+using FarLibCL.Authors.Dtos;
+using FarLibCL.Authors.Entities;
+using FarLibCL.Books.Dtos;
+using FarLibCL.Books.Entities;
+
+namespace FarLibBLL.Books.Mappers;
+
+public class BookMapper : IBookMapper
+{
+    public Book MapAddDtoToEntity(AddBookDto dto)
+    {
+        return new Book()
+        {
+            Id = Guid.Empty,
+            Title = dto.Title,
+            Description = dto.Description,
+            Type = dto.Type,
+            Category = dto.Category
+        };
+    }
+
+    public BookDetailsDto MapEntityToDetailsDto(Book entity)
+    {
+        return new BookDetailsDto()
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            Description = entity.Description,
+            Type = entity.Type,
+            Category = entity.Category,
+            Authors = [.. entity.Authors.Select(a => new BookAuthorListItemDto()
+            {
+                Id = a.Id,
+                Name = a.Name,
+            })]
+        };
+    }
+
+    public BookListItemDto MapEntityToListItemDto(Book entity)
+    {
+        return new BookListItemDto()
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            Authors = [.. entity.Authors.Select(a => new BookAuthorListItemDto(){
+                Id = a.Id,
+                Name = a.Name
+            })]
+        };
+    }
+}
