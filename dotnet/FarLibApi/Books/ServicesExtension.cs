43d1527bcfd2bac41f1122fdbbd7703165f4ab3b
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-24 18:29:08 +0100

    [Added] IBookService and IBookMapper to DI container

--- dotnet/FarLibApi/Books/ServicesExtension.cs
+++ dotnet/FarLibApi/Books/ServicesExtension.cs
@@ -1,3 +1,7 @@
+using FarLibBLL.Books.Mappers;
+using FarLibBLL.Books.Mappers.Interfaces;
+using FarLibBLL.Books.Services;
+using FarLibBLL.Books.Services.Interfaces;
 using FarLibDAL.Books.Repositories;
 using FarLibDAL.Books.Repositories.Interfaces;
 
@@ -8,5 +12,8 @@ public static class ServicesExtension
     public static void AddBookServices(this IServiceCollection services)
     {
         services.AddScoped<IBookRepository, BookRepository>();
+
+        services.AddScoped<IBookService, BookService>();
+        services.AddSingleton<IBookMapper, BookMapper>();
     }
 }
