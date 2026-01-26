dc46c0f03ce95a285db7cad33836a700e07368ab
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-26 16:33:37 +0100

    [Added] IDistributorService, IDistributorMapper to DI container

--- dotnet/FarLibApi/Distributors/ServicesExtension.cs
+++ dotnet/FarLibApi/Distributors/ServicesExtension.cs
@@ -1,3 +1,7 @@
+using FarLibBLL.Distributors.Mappers;
+using FarLibBLL.Distributors.Mappers.Interfaces;
+using FarLibBLL.Distributors.Services;
+using FarLibBLL.Distributors.Services.Interfaces;
 using FarLibDAL.Distributors.Repositories;
 using FarLibDAL.Distributors.Repositories.Interfaces;
 
@@ -8,5 +12,8 @@ public static class ServicesExtension
     public static void AddDistributorServices(this IServiceCollection services)
     {
         services.AddScoped<IDistributorRepository, DistributorRepository>();
+
+        services.AddScoped<IDistributorService, DistributorService>();
+        services.AddSingleton<IDistributorMapper, DistributorMapper>();
     }
 }
