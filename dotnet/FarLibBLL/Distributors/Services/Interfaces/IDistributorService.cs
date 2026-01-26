49e656df3f8f2e48f12d087bdd5b382ab01457f6
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-26 16:33:15 +0100

    [Added] IDistributorService, IDistributorMapper interfaces, implementation and required dtos and constants

---
+++ dotnet/FarLibBLL/Distributors/Services/Interfaces/IDistributorService.cs
@@ -0,0 +1,12 @@
+using FarLibCL.Distributors.Dtos;
+
+namespace FarLibBLL.Distributors.Services.Interfaces;
+
+public interface IDistributorService
+{
+    Task AddAsync(AddDistributorDto add);
+    Task<DistributorDetailsDto?> GetByIdAsync(Guid id);
+    Task<DistributorListDto> GetAllAsync(DistributorFilterDto filter);
+    Task UpdateAsync(Guid id, UpdateDistributorDto update);
+    Task DeleteAsync(Guid id);
+}
