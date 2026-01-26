487fb62758b4bf610c9c123f1f8a36ba96680336
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-26 13:41:22 +0100

    [Added] author information for adding book

--- dotnet/FarLibCL/Authors/Entities/Author.cs
+++ dotnet/FarLibCL/Authors/Entities/Author.cs
@@ -5,7 +5,7 @@ namespace FarLibCL.Authors.Entities;
 public class Author
 {
     public Guid Id { get; set; }
-    public required string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
 
     public IList<Book> Books { get; set; } = [];
