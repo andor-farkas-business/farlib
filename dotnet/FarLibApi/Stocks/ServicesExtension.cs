97583e95d6b23c1da129d9452f9672e5b0bedbec
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-27 14:58:02 +0100

    [Added] IStockService, IStockMapper to DI container

--- dotnet/FarLibApi/Stocks/ServicesExtension.cs
+++ dotnet/FarLibApi/Stocks/ServicesExtension.cs
@@ -1,3 +1,7 @@
+using FarLibBLL.Stocks.Mappers;
+using FarLibBLL.Stocks.Mappers.Interfaces;
+using FarLibBLL.Stocks.Services;
+using FarLibBLL.Stocks.Services.Interfaces;
 using FarLibDAL.Stocks.Repositories;
 using FarLibDAL.Stocks.Repositories.Interfaces;
 
@@ -8,5 +12,8 @@ public static class ServicesExtension
     public static void AddStockServices(this IServiceCollection services)
     {
         services.AddScoped<IStockRepository, StockRepository>();
+
+        services.AddScoped<IStockService, StockService>();
+        services.AddSingleton<IStockMapper, StockMapper>();
     }
 }
