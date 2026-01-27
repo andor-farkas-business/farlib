32dd07e887ffda4402aa597f7adaac7150b2db25
Andor Farkas <farkas.andor.job@gmail.com>
2026-01-27 17:35:32 +0100

    [Changed] ObjectNotFoundException to extend FarLibException

--- dotnet/FarLibCL/Exceptions/ObjectNotFoundException.cs
+++ dotnet/FarLibCL/Exceptions/ObjectNotFoundException.cs
@@ -1,8 +1,22 @@
+using System.Net;
+using FarLibCL.Exceptions.Enums;
+
 namespace FarLibCL.Exceptions;
 
 public class ObjectNotFoundException(
     string objectType,
     string propertyName,
-    string propertyValue) : Exception($"The object '{objectType}' with the '{propertyName}':'{propertyValue}' was not found.")
+    string propertyValue) : FarLibException($"The object '{objectType}' with the '{propertyName}':'{propertyValue}' was not found.")
 {
+    public override ExceptionData ExceptionData => new()
+    {
+        StatusCode = HttpStatusCode.NotFound,
+        ErrorType = ErrorType.ObjectNotFound,
+        Error = new Dictionary<string, string>
+        {
+            { "objectType", objectType },
+            { "propertyName", propertyName },
+            { "propertyValue", propertyValue }
+        }
+    };
 }
