07adb8da06b60c3684d1c7b8bf2c22148ddfcb09
Andor Farkas <farkas.andor.job@gmail.com>
2026-03-20 17:39:55 +0100

    [Added] ServicesExtension classes for author, book, distributor and stock entities

---
+++ dotnet/FarLibPages/Books/ServicesExtension.cs
@@ -0,0 +1,19 @@
+using FarLibBLL.Books.Mappers;
+using FarLibBLL.Books.Mappers.Interfaces;
+using FarLibBLL.Books.Services;
+using FarLibBLL.Books.Services.Interfaces;
+using FarLibDAL.Books.Repositories;
+using FarLibDAL.Books.Repositories.Interfaces;
+
+namespace FarLibPages.Books;
+
+public static class ServicesExtension
+{
+    public static void AddBookServices(this IServiceCollection services)
+    {
+        services.AddScoped<IBookRepository, BookRepository>();
+
+        services.AddScoped<IBookService, BookService>();
+        services.AddSingleton<IBookMapper, BookMapper>();
+    }
+}
